83d71651445e10b1868deee7bf32f25399afd93b
des <deskell98@gmail.com>
2021-03-05 16:51:58 +0000

    finished bidirectional motion

--- code/interp.py
+++ code/interp.py
@@ -10,14 +10,14 @@ from scipy import signal
 from sep_auto import generate_toeplitz, generate_ac_tensor, ac_tensor_uni
 from motion import motion_est
 import math
-
+from cuda_auto import get_c, get_C
 @jit(nopython=True)
 def reverse_points(mvs):
     reverse_mvs = np.zeros(mvs.shape)
     for i in range(mvs.shape[0]):
         for j in range(mvs.shape[1]):
             reverse_mvs[i - mvs[i, j, 0], j - mvs[i, j, 1]] = mvs[i, j] 
-    return reverse_mvs
+    return reverse_mvs.astype(np.int64)
 
 def get_psnr(image1, image2):
     mse = np.sum((image1 - image2) ** 2)/(image1.shape[0] * image2.shape[1])
@@ -42,8 +42,30 @@ def generate_Q_auto(width):
     Q = Q.reshape((width * width, 2)).astype(np.int64)
     return Q
 
+def generate_Q2(width):
+    q_len = 2 * width - 1
+    Q = np.zeros((2 * q_len, 3))
+    #maximum offset
+    off = int(width / 2)
+    #generate 1d array of offset vectors
+    #for kernel width of 3 should be [-1,0], [0,0], [1,0], [0, -1], [0,0] -- plus shape without repeating origin
+    for i in range(0, width):
+        Q[i] = np.array([i - off, 0, -1])
+        Q[i + q_len] = np.array([i - off, 0, 1])
+
+    for i in range(0, off):
+        Q[width + i] = np.array([0, i - off, -1])
+        Q[width + i + q_len] = np.array([0, i - off, 1])
+    
+    for i in range(0, off):
+        Q[width + off + i] = np.array([0, i + 1, -1])
+        Q[width + off + i + q_len] = np.array([0, i + 1, 1])
+    Q = Q.astype(np.int64)
+    return Q
+
 def generate_Q(width):
-    Q = np.zeros((2 * width - 1, 2))
+    q_len = 2 * width - 1
+    Q = np.zeros((q_len, 2))
     #maximum offset
     off = int(width / 2)
     #generate 1d array of offset vectors
@@ -75,24 +97,6 @@ def calc_a(x1, x2, Q, toeplitz1, toeplitz2, toeplitz3, autocor, q_d, k_width):
     #print(a.shape)
     return a
 
-@jit(nopython=True)
-def calc_a_motion(x1, x2, Q, toeplitz1, toeplitz2, toeplitz3, autocor1, autocor2, q_d, qm_d, qt_d, k_width, mvs1, mvs2, mvs3):
-    #autocorrelation
-    q_len = 2 * k_width - 1
-    c = np.zeros(2 * q_len)
-    C = np.zeros((2 * q_len, 2 * q_len))
-    for i in range(q_len):
-        c[i]         = autocor1[x1, x2, qm_d[(int(Q[i, 0] - mvs1[x1, x2, 0]), int(Q[i, 1] - mvs1[x1, x2, 1]))]]
-        c[i + q_len] = autocor2[x1, x2, qm_d[(int(Q[i, 0] - mvs2[x1, x2, 0]), int(Q[i, 1] - mvs2[x1, x2, 1]))]]
-        for j in range(q_len):
-            offset = Q[j] - Q[i]
-            C[i, j]                           = toeplitz1[x1 + Q[i, 0] - mvs1[x1, x2, 0], x2 + Q[i, 1] - mvs1[x1, x2, 1], q_d[(offset[0], offset[1])]]
-            C[j, i + q_len] = C[i + q_len, j] = toeplitz2[x1 + Q[i, 0] - mvs2[x1, x2, 0], x2 + Q[i, 1] - mvs2[x1, x2, 1], \
-                 qt_d[(int(offset[0] - mvs1[x1, x2, 0] + mvs2[x1, x2, 0]),\
-                       int(offset[1] - mvs1[x1, x2, 1] + mvs2[x1, x2, 1]))]]
-            C[i + q_len, j + q_len]           = toeplitz3[x1 + Q[i, 0] - mvs2[x1, x2, 0], x2 + Q[i, 1] - mvs2[x1, x2, 1], q_d[(offset[0], offset[1])]]
-    a = np.linalg.lstsq(C.astype(np.float32), c.astype(np.float32))[0]
-    return a
 
 @jit(nopython=True)
 def estimate_frame(I1, I2, A, k_width, k_off, b):
@@ -161,12 +165,12 @@ def estimate_frame_motion(I1, I2, A, k_width, k_off, b, mvs1, mvs2):
                 predicted[i, j, channel] = np.sum(mask)
     return predicted
 
-def estimate_coefficients_motion(I, toeplitz1, toeplitz2, toeplitz3, autocor1, autocor2, Q, q_d, qm_d, qt_d, k_width, b, mvs1, mvs2, mvs3):  
-    A = np.zeros((I.shape[0] - 2 * b, I.shape[1] - 2 * b, 4 * k_width - 2))
+def estimate_coefficients_motion(c_array, C_array):  
+    A = np.zeros(c_array.shape)
     print("Estimating coefficients...")
-    for i in tqdm(range(0, A.shape[0])):
+    for i in range(0, A.shape[0]):
         for j in range(0, A.shape[1]):
-            A[i, j] = calc_a_motion(i + b, j + b, Q, toeplitz1, toeplitz2, toeplitz3, autocor1, autocor2, q_d, qm_d, qt_d, k_width, mvs1, mvs2, mvs3)
+            A[i, j] = np.linalg.lstsq((C_array[i, j]).astype(np.float32), c_array[i,j].astype(np.float32))[0]
     return A
 
 def estimate_coefficients(I, toeplitz1, toeplitz2, toeplitz3, autocor, Q, q_d, k_width, b):  
@@ -182,7 +186,6 @@ def predict_frame(image1, image2, image3, k_width, ac_block, motion):
     k_off  = int(k_width  / 2)
     max_motion = 10
     b      = int(ac_block / 2) + int(k_width / 2) + max_motion
-    ac_size = k_width * int(1 + max_motion/k_off) - 1
 
     I1 = cv2.copyMakeBorder(cv2.imread(image1), b, b, b, b, cv2.BORDER_REFLECT).astype(np.int32)
     I2 = cv2.copyMakeBorder(cv2.imread(image2), b, b, b, b, cv2.BORDER_REFLECT).astype(np.int32)
@@ -196,29 +199,25 @@ def predict_frame(image1, image2, image3, k_width, ac_block, motion):
     if(motion == 1):
         mvs1 = motion_est(i1, i2, b, max_motion)
         mvs2 = motion_est(i3, i2, b, max_motion)
-        mvs3 = reverse_points(mvs1)
-        qm_d = q_dict(generate_Q_auto(ac_size))
-        qt_d = q_dict(generate_Q_auto(2 * ac_size - 1))
-
-    print("Generating toeplitzs")
-    toeplitz1 = generate_toeplitz(i1, i1, ac_block, k_width, double_Q, b)
-    toeplitz3 = generate_toeplitz(i3, i3, ac_block, k_width, double_Q, b)
+        i_tensor = np.zeros((i1.shape[0], i1.shape[1], 3))
+        i_tensor[:, :, 0] = i1
+        i_tensor[:, :, 1] = i2
+        i_tensor[:, :, 2] = i3
+   
     if(motion):
-        toeplitz2 = generate_toeplitz(i3, i1, ac_block, k_width, generate_Q_auto(2 * ac_size - 1), b)
-        autocor1  = ac_tensor_uni(i1, i2, ac_block, k_width, generate_Q_auto(ac_size), b)
-        autocor2  = ac_tensor_uni(i3, i2, ac_block, k_width, generate_Q_auto(ac_size), b)
-        A = estimate_coefficients_motion(I2[:, :, 1], toeplitz1, toeplitz2, toeplitz3, autocor1, autocor2, Q, q_d, qm_d, qt_d, k_width, b, mvs1, mvs2, mvs3)
+        c_array = get_c(i_tensor, Q, int(ac_block/2), mvs1, mvs2, b)
+        C_array = get_C(i_tensor, Q, int(ac_block/2), mvs1, mvs2, b)
+        A = estimate_coefficients_motion(c_array, C_array)
         predicted = estimate_frame_motion(I1, I3, A, k_width, k_off, b, mvs1, mvs2)
     else:
+        print("Generating toeplitzs")
+        toeplitz1 = generate_toeplitz(i1, i1, ac_block, k_width, double_Q, b)
+        toeplitz3 = generate_toeplitz(i3, i3, ac_block, k_width, double_Q, b)
         toeplitz2 = generate_toeplitz(i3, i1, ac_block, k_width, double_Q, b)
         autocor2  = generate_ac_tensor(i2, i1, i3, ac_block, k_width, Q, b)
         A = estimate_coefficients(I2[:, :, 1], toeplitz1, toeplitz2, toeplitz3, autocor2, Q, q_d, k_width, b)
         predicted = estimate_frame(I1, I3, A,  k_width, k_off, b)
 
-    with open('a.npy', 'wb') as a_file:
-        np.save(a_file, A)
-    predicted = 0
-    predicted = estimate_frame(I1, I3, A,  k_width, k_off, b)
     predicted[predicted > 255] = 255
     predicted[predicted < 0] = 0
     return predicted
@@ -239,8 +238,8 @@ def main():
         image2 = '../images/image1.png'
         image3 = '../images/image2.png'
         out = '../output/out.png'
-        k_width = 3
-        ac_block = 5
+        k_width = 5
+        ac_block = 11
         motion = 1
     print("Kernel size:", k_width)
     print("Autocorrelation kernel size:", ac_block)
@@ -248,7 +247,9 @@ def main():
     
     print("Predicting frames")
     predicted = predict_frame(image1, image2, image3, k_width, ac_block, motion)
-    print("PSNR is :", get_psnr(cv2.imread(image2), predicted))
+    print("PSNR1 is :", get_psnr(cv2.imread(image1), predicted))
+    print("PSNR2 is :", get_psnr(cv2.imread(image2), predicted))
+    print("PSNR3 is :", get_psnr(cv2.imread(image3), predicted))
     if(cv2.imwrite(out, predicted) == False):
         print("Error writing file!")
     else:
